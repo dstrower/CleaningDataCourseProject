493d2b7e911c4036fb51879a42eefcfba195d19f
Dave Trower <dstrower@gmail.com>
2014-08-16 15:24:34 -0500

    Combined X and Y data sets

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,15 @@
 # Main R script
+
+main <- function() {
+  dataFrameTrain <- getSet("train")
+}
+
+getSet <- function(setName) {
+   XDataFrame <- dataFrameOfX(setName)
+   YDataframe <- dataFrameOfY(setName)
+   completeSet <- cbind(YDataframe,XDataFrame)
+}
+
 dataFrameOfX <- function(x) {
   rootDir = "UCI HAR Dataset"    
   xDataFileLocation = paste(rootDir,x,"X_train.txt",sep="/")
@@ -12,4 +23,21 @@ dataFrameOfX <- function(x) {
     featureVector[i] = as.character(featuresArray$V2[i])
   }
   names(x)  <- featureVector
+  x
+}
+
+dataFrameOfY <- function(setType) {
+  rootDir = "UCI HAR Dataset"    
+  yDataFileLocation = paste(rootDir,setType,"Y_train.txt",sep="/")
+  y <- read.table(yDataFileLocation)
+  names(y) <- c("Activity")
+  activityLabelsFileLocation =  paste(rootDir,"activity_labels.txt",sep="/")
+  actvityDesc = read.table(activityLabelsFileLocation)
+  names(actvityDesc) = c("RowNumber","Description")
+  for(i in 1:nrow(y)) {  
+    index = as.numeric(y$Activity[i])
+    description = as.character(actvityDesc$Description[index])
+    y$Activity[i] <- description
+  }
+  y
 }
