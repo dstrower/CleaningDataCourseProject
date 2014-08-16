723c52a146491c1838c692972d452821b01fd47e
Dave Trower <dstrower@gmail.com>
2014-08-16 12:29:40 -0500

    Added to main script so that it creates a dataFrame with description.

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,15 @@
 # Main R script
+dataFrameOfX <- function(x) {
+  rootDir = "UCI HAR Dataset"    
+  xDataFileLocation = paste(rootDir,x,"X_train.txt",sep="/")
+  x <- read.table(xDataFileLocation)
+  featuresFileLocation = paste(rootDir,"features.txt",sep="/");
+  featuresArray <- read.table(featuresFileLocation)
+  featureSize <- nrow(featuresArray)
+  vec <- c()
+  featureVector <- c(vec, 1:featureSize)
+  for(i in 1:featureSize) {      
+    featureVector[i] = as.character(featuresArray$V2[i])
+  }
+  names(x)  <- featureVector
+}
